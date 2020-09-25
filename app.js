cac5daecee9636f05d288f2ee4533c08fea48932
jsi-webdev <j.skullow@gmail.com>
2020-09-25 15:40:20 +0900

    R6 - LocalStorage : store addTask in LS

--- app.js
+++ app.js
@@ -42,13 +42,25 @@ function addTask(e) {
   // Append the link to li
   li.appendChild(link);
   // Store in LS
-
+  storeTaskInLocalStorage(taskInput.value);
   // Append li to ul
   taskList.appendChild(li);
   // Clear input
   taskInput.value = "";
   // console.log(li);
   e.preventDefault();
+}
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // Remove Task
